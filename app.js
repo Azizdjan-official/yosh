4b1d839f5ee11ae636091f666d0c5a6ef70cdc05
azizdjan_official <tulaganovazizjon@gmail.com>
2023-10-25 00:20:09 +0500

    done

---
+++ app.js
@@ -0,0 +1,6 @@
+let age = prompt('Yoshingiz nechchida ?')
+let yil = 2023 - age;
+alert( ' Siz ' + yil + ' yilsiz '  + ' va  3 yildan keyin '  + (+age + 3) + ' yoshga kirasiz ' );
+
+let yoshiz = prompt("yoshni kiriting");
+alert( yoshiz * 1 - age * 1  )
